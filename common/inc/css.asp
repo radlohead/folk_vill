819180783bb0da4a44ea6913e33bf5c0d2f5deaa
radlo <radlohead7@naver.com>
2016-07-29 22:05:44 +0900

    폼수정1일차

--- common/inc/css.asp
+++ common/inc/css.asp
@@ -2,4 +2,5 @@
 <link rel="stylesheet" type="text/css" href="/mobile/common/css/swiper.min.css" />
 <link rel="stylesheet" type="text/css" href="/mobile/common/css/slick.css" />
 <link rel="stylesheet" type="text/css" href="/mobile/common/css/index.css" />
+<link rel="stylesheet" type="text/css" href="/mobile/common/css/form.css" />
 <link rel="stylesheet" type="text/css" href="/mobile/common/css/media.css" />
--- common/js/form.js
+++ common/js/form.js
@@ -32,6 +32,13 @@ $(document).ready(function(){
             var select_name = $(this).children("option:selected").text();
             $(this).siblings(".email_02").val(select_name);
         });
+
+        var select_email2 = $("select#email3");
+        select_email2.change(function(){
+            var select_name2 = $(this).find("option:selected").text();
+            $(this).is("option:first-child").text(select_name2);
+            console.log(select_name2)
+        });
     }
     select_box();
 
